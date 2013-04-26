ee4000f1815548c0043dea09048f4bb34e8b5fd2
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-04-26 12:51:46 +0200

    equal method

--- lib/src/objects.dart
+++ lib/src/objects.dart
@@ -7,7 +7,7 @@ class Objects {
       (fields == null || fields.isEmpty) ? 0 : fields.fold(1, (int previous, Object e) => 31* previous + (e == null ? 0 : e.hashCode));
   
   
-  
+  static bool equal(Object a, Object b) => a == b || (a != null && a == b);
   
 }
 
--- test/objects_test.dart
+++ test/objects_test.dart
@@ -2,29 +2,54 @@ import 'package:papaya/papaya.dart';
 import 'package:unittest/unittest.dart';
 
 main(){
-  test('Null object have null hashcode', () {
-    int hashcode = Objects.hashcode(null);
-    expect(hashcode, 0);
-  });  
-  test('Empty object have null hashcode', () {
-    int hashcode = Objects.hashcode([]);
-    expect(hashcode, 0);
-  });    
-  test('Should have hascode', () {
-    int hashcode = Objects.hashcode(["Hello"]);
-    expect(hashcode, isNotNull);
+  group('Hashcode', () {
+    test('Null object have hashcode 0', () {
+      int hashcode = Objects.hashcode(null);
+      expect(hashcode, 0);
+    });  
+    test('Empty object have hashcode 0', () {
+      int hashcode = Objects.hashcode([]);
+      expect(hashcode, 0);
+    });    
+    test('Should have hascode', () {
+      int hashcode = Objects.hashcode(["Hello"]);
+      expect(hashcode, isNotNull);
+    });
+    test('Should have same hascode', () {
+      int hashcode1 = Objects.hashcode(["Hello", "world"]);
+      int hashcode2 = Objects.hashcode(["Hello", "world"]);
+      expect(hashcode1, isNotNull);
+      expect(hashcode2, isNotNull);
+      expect(hashcode1, equals(hashcode2));
+    });  
+    test('Should have specific hascode', () {
+      int hashcode = Objects.hashcode(["Hello", "World"]);
+      expect(hashcode, isNotNull);
+      expect(hashcode, equals(26697196704));
+    });
   });
-  test('Should have same hascode', () {
-    int hashcode1 = Objects.hashcode(["Hello", "world"]);
-    int hashcode2 = Objects.hashcode(["Hello", "world"]);
-    expect(hashcode1, isNotNull);
-    expect(hashcode2, isNotNull);
-    expect(hashcode1, equals(hashcode2));
-  });  
-  test('Should have specific hascode', () {
-    int hashcode = Objects.hashcode(["Hello", "World"]);
-    expect(hashcode, isNotNull);
-    expect(hashcode, equals(26697196704));
+  
+  group('Eauals', () {
+    test('Null objects are equals', () {
+      bool result = Objects.equal(null, null);
+      expect(result, isTrue);
+    });
+    test('Object cannot be equals to nul', () {
+      bool result1 = Objects.equal("Hello", null);
+      bool result2 = Objects.equal(null, "Hello");
+      expect(result1, isFalse);
+      expect(result2, isFalse);
+    });
+    test('Are equals', () {
+      bool result = Objects.equal("Hello", "Hello");
+      expect(result, isTrue);
+    }); 
+    test('Are be different', () {
+      bool result = Objects.equal("Hello", "hello");
+      expect(result, isFalse);
+    }); 
   });
+  
+  
 }
 
